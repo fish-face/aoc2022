31f95064281011ff3322ccb015eb63165af65357
Chris Le Sueur <c.m.lesueur@gmail.com>
2022-12-05 21:55:03 +0000

    Solve day 5

--- src/bin/day-05.rs
+++ src/bin/day-05.rs
@@ -4,6 +4,18 @@ use std::collections::VecDeque;
 use regex::Regex;
 use aoc2022::common::read_input_lines;
 
+fn move_crates(stacks: &mut Vec<VecDeque<char>>, from: usize, to: usize, count: usize, reverse: bool) {
+    let fromstack = &mut stacks[from];
+    let mut moving = fromstack.split_off(fromstack.len() - count);
+    if reverse {
+        for v in moving.make_contiguous().iter().rev() {
+            stacks[to].push_back(*v);
+        }
+    } else {
+        stacks[to].append(&mut moving);
+    }
+}
+
 fn main() {
     let mut lines = read_input_lines().expect("Could not read input");
     let stacks_str = lines.by_ref().take_while(|line| line != "").collect::<Vec<_>>();
@@ -12,19 +24,16 @@ fn main() {
     let mut stacks1:Vec<VecDeque<char>> = Vec::new();
     for _ in 0..n {
         stacks1.push(VecDeque::new());
-        // stacks2.push(VecDeque::new());
     }
     for stack_line in &stacks_str[..stacks_str.len()-1] {
         for i in 0..n {
             let c = stack_line.as_bytes()[(i * 4) + 1] as char;
             if c != ' ' {
                 stacks1[i].push_front(c);
-                // stacks2[i].push_front(c);
             }
         }
     }
     let mut stacks2 = stacks1.clone();
-    // println!("{:?}", stacks);
     let r = Regex::new(r"move ([0-9]+) from ([0-9]+) to ([0-9]+)").expect("Instruction regex did not compile");
 
     for instruction in lines {
@@ -33,24 +42,20 @@ fn main() {
             .expect("Line did not parse")
             .iter()
             .skip(1)
-            .map(|cap| cap.unwrap().as_str().parse::<usize>().unwrap_or_else(|v| panic!("{} {}", v, instruction)))
+            .map(|cap|
+                cap.unwrap().as_str().parse::<usize>().unwrap_or_else(|v| panic!("{} {}", v, instruction))
+            )
             .collect::<Vec<_>>();
         if let [count, from, to] = &captures[..] {
-            // println!("{}", instruction);
-            let fromstack = &mut stacks2[*from - 1];
-            let len = fromstack.len();
-            for v in fromstack.make_contiguous()[len - count - 1..len].iter() {
-                stacks2[*to - 1].push_back(*v);
-            }
-            for _ in 0..*count {
-                let v = stacks1[*from - 1].pop_back().expect("Stack was empty");
-                stacks1[*to - 1].push_back(v);
-                fromstack.pop_back().expect("Stack 2 was empty");
-            }
+            move_crates(&mut stacks1, *from - 1, *to - 1, *count, true);
+            move_crates(&mut stacks2, *from - 1, *to - 1, *count, false);
         } else {
             panic!("wut");
         }
     }
-    println!("{}", stacks1.iter().map(|stack| stack[stack.len() - 1].to_string()).collect::<Vec<_>>().join(""));
-    println!("{}", stacks2.iter().map(|stack| stack[stack.len() - 1].to_string()).collect::<Vec<_>>().join(""));
+    fn format_output(stacks: Vec<VecDeque<char>>) {
+        println!("{}", stacks.iter().map(|stack| stack[stack.len() - 1].to_string()).collect::<Vec<_>>().join(""));
+    }
+    format_output(stacks1);
+    format_output(stacks2);
 }

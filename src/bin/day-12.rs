e47463b196619e192b22fdc77ea62f7ad1422010
Chris Le Sueur <c.m.lesueur@gmail.com>
2022-12-12 21:22:10 +0000

    Solve day 12 part 2

--- src/bin/day-12.rs
+++ src/bin/day-12.rs
@@ -1,11 +1,8 @@
-use petgraph::{Directed, Graph};
-use petgraph::algo::dijkstra;
-use petgraph::graph::{NodeIndex, UnGraph};
 use aoc2022::common::read_input_lines;
 use aoc2022::coord::Pt;
 use aoc2022::grid::Grid;
-
-type Coord = Pt<usize>;
+use petgraph::algo::dijkstra;
+use petgraph::{Directed, Graph};
 
 fn convert_ends(c: u8) -> u8 {
     match c as char {
@@ -27,7 +24,9 @@ fn main() {
     let mut end = None;
     let mut graph = Graph::<u8, (), Directed>::new();
 
+    // Create graph nodes and store their indices somewhere
     let graph_grid = grid.map(|v: &u8| graph.add_node(*v));
+    // Create graph edges, and also search for the start and end
     for (p, i) in graph_grid.enumerate() {
         let mut v = grid[p];
         if v == 'E' as u8 {
@@ -36,16 +35,18 @@ fn main() {
             start = Some(i);
         }
         v = convert_ends(v);
-        // println!("{} {}: {}", x, y, v);
         // AHAHAHAHA I LOVE RUST
         let p_isize = Pt(p.0 as isize, p.1 as isize);
         for neighbour in p_isize.neighbours4() {
-            if neighbour.0 < 0 || neighbour.1 < 0 {continue;}
+            if neighbour.0 < 0 || neighbour.1 < 0 {
+                continue;
+            }
             // AHAHAHAHA I LOVE RUST
             let neighbour = Pt(neighbour.0 as usize, neighbour.1 as usize);
             if let (Ok(u), Ok(j)) = (grid.get(neighbour), graph_grid.get(neighbour)) {
                 let u = convert_ends(*u);
-                if v + 1 >= u {
+                // This condition is backwards because for part2 we have to search from the goal to several different places
+                if u + 1 >= v {
                     graph.add_edge(*i, *j, ());
                 }
             }
@@ -53,12 +54,15 @@ fn main() {
     }
     let start = start.unwrap();
     let end = end.unwrap();
-    // println!("{:?} {:?}", start, end);
-    // println!("{}", grid.to_string(None));
-    // println!("{}", graph_grid.map(|i| i.index()).to_string(None));
-    // println!("{:?}", graph);
-    let dijkstra_map = dijkstra(&graph, *start, Some(*end), |_| 1);
-    // println!("{:#?}", dijkstra_map.iter().map(|(k, v)| format!("{} {}", k.index(), v)).collect::<Vec<_>>());
-    println!("{:?}", dijkstra_map.get(end).expect("No route found"));
-    // println!("{}", grid.to_string(Some(" ")))
+    // Search from the goal backwards so that we can find any valid starting position in part 2
+    let dijkstra_map = dijkstra(&graph, *end, None, |_| 1);
+    println!("{:?}", dijkstra_map.get(start).expect("No route found"));
+    println!(
+        "{:?}",
+        grid.enumerate()
+            .filter(|(_, v)| **v == 'a' as u8)
+            .map(|(p, _)| dijkstra_map.get(&graph_grid[p]).unwrap_or(&9001))
+            .min()
+            .expect("No route found")
+    );
 }

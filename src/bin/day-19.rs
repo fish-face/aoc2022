26bb4b3c914142b25f17409f6bbbaa1dc82ee51e
Chris Le Sueur <c.m.lesueur@gmail.com>
2022-12-25 18:33:40 +0000

    Solve day 19

--- src/bin/day-19.rs
+++ src/bin/day-19.rs
@@ -1,17 +1,31 @@
-use std::cmp::{max, min};
-use std::collections::{HashMap, VecDeque};
 use aoc2022::common::read_input;
-use anyhow::{Error, Result};
+use std::cmp::{max, min, Ordering};
+use std::collections::{HashMap, VecDeque};
+use std::slice::Iter;
+
+#[derive(Copy, Clone)]
+enum Resource {
+    Ore,
+    Clay,
+    Obsidian,
+    Geode,
+}
+
+use Resource::*;
+
+impl Resource {
+    pub fn iterator() -> Iter<'static, Resource> {
+        static RESOURCES: [Resource; 4] = [Ore, Clay, Obsidian, Geode];
+        RESOURCES.iter()
+    }
+}
+
+type Res = [usize; 4];
 
 #[derive(Debug)]
 pub struct Scenario {
     id: usize,
-    ore_robot_ore: usize,
-    clay_robot_ore: usize,
-    obsidian_robot_ore: usize,
-    obsidian_robot_clay: usize,
-    geode_robot_ore: usize,
-    geode_robot_obsidian: usize,
+    costs: [Res; 4],
 }
 
 peg::parser! {
@@ -28,12 +42,12 @@ peg::parser! {
             " ore and " geode_robot_obsidian:num()" obsidian." {
             Scenario{
                 id: id,
-                ore_robot_ore: ore_robot,
-                clay_robot_ore: clay_robot,
-                obsidian_robot_ore: obs_robot_ore,
-                obsidian_robot_clay: obs_robot_clay,
-                geode_robot_ore: geode_robot_ore,
-                geode_robot_obsidian: geode_robot_obsidian,
+                costs: [
+                    [ore_robot, 0, 0, 0],
+                    [clay_robot, 0, 0, 0],
+                    [obs_robot_ore, obs_robot_clay, 0, 0],
+                    [geode_robot_ore, 0, geode_robot_obsidian, 0],
+                ]
             }
         }
         rule _()
@@ -44,72 +58,45 @@ peg::parser! {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct RobotState {
-    ore_robots: usize,
-    clay_robots: usize,
-    obsidian_robots: usize,
-    geode_robots: usize,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct State {
     time: usize,
-    robots: RobotState,
-    ore: usize,
-    clay: usize,
-    obsidian: usize,
-    geodes: usize,
+    robots: Res,
+    resources: Res,
 }
 
 impl State {
     fn new(ore_robots: usize) -> Self {
-        State{
+        State {
             time: 0,
-            robots: RobotState{
-                ore_robots,
-                clay_robots: 0,
-                obsidian_robots: 0,
-                geode_robots: 0,
-            },
-            ore: 0,
-            clay: 0,
-            obsidian: 0,
-            geodes: 0,
+            robots: [ore_robots, 0, 0, 0],
+            resources: [0, 0, 0, 0],
         }
     }
 }
 
-fn better(a: &State, b: &State) -> bool {
-    a.time <= b.time &&
-        // heuristic(a, 24) >= heuristic(b, 24)
-        a.ore >= b.ore &&
-        a.clay >= b.clay &&
-        a.obsidian >= b.obsidian &&
-        a.geodes >= b.geodes
+impl PartialOrd<Self> for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
-type Heuristic = (usize);
-
-fn heuristic(state: &State, max_time: usize) -> Heuristic {
-    let t = max_time - state.time;
-    (
-        // state.time,
-        state.ore + t * state.robots.ore_robots +
-            state.clay + t * state.robots.clay_robots +
-            state.obsidian + t * state.robots.obsidian_robots +
-            state.geodes + t * state.robots.geode_robots
-    )
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.resources.cmp(&other.resources)
+    }
 }
 
 fn advance(state: &mut State, t: usize) {
     state.time += t;
-    state.ore = state.ore + state.robots.ore_robots * t;
-    state.clay = state.clay + state.robots.clay_robots * t;
-    state.obsidian = state.obsidian + state.robots.obsidian_robots * t;
-    state.geodes = state.geodes + state.robots.geode_robots * t;
+    state
+        .resources
+        .iter_mut()
+        .zip(state.robots)
+        .for_each(|(res, robots)| *res += robots * t)
 }
 
-pub const fn div_ceil(lhs: usize, rhs: usize) -> Option<usize> {
+// Why is this not a std function?
+pub const fn checked_div_ceil(lhs: usize, rhs: usize) -> Option<usize> {
     if rhs == 0 {
         None
     } else {
@@ -123,186 +110,91 @@ pub const fn div_ceil(lhs: usize, rhs: usize) -> Option<usize> {
     }
 }
 
-fn build_ore(from: &State, scenario: &Scenario) -> Option<State> {
+fn build_robot(from: &State, scenario: &Scenario, robot_kind: Resource) -> Option<State> {
     let mut build = from.clone();
-    let res_need = scenario.ore_robot_ore.saturating_sub(build.ore);
-    let time = match res_need > 0 {
-        true => div_ceil(res_need, build.robots.ore_robots)?,
-        false => 1
-    };
-    advance(&mut build, time);
-    build.ore -= scenario.ore_robot_ore;
-    build.robots.ore_robots += 1;
-    Some(build)
-    // time.div_ceil();
-}
+    let robot_kind = robot_kind as usize;
+    // The number of each type of resource needed to build the selected robot
+    let res_need = scenario.costs[robot_kind]
+        .iter()
+        .zip(build.resources)
+        .map(|(cost, res)| cost.saturating_sub(res));
+    // The time needed to acquire each kind of resource, or short circuit and return None if
+    // any required resource has no builders
+    let time = res_need
+        .zip(build.robots)
+        .map(|(need, robots)| {
+            Some(match need > 0 {
+                // divide needed resource by builders, rounded up, or short circuit if no builders
+                true => checked_div_ceil(need, robots)?,
+                // if we don't need any, we don't need builders
+                false => 0,
+            })
+        })
+        .try_fold(0_usize, |prev, next| next.map(|ok| max(prev, ok)))?
+        + 1;
 
-fn build_clay(from: &State, scenario: &Scenario) -> Option<State> {
-    let mut build = from.clone();
-    let res_need = scenario.clay_robot_ore.saturating_sub(build.ore);
-    let time = match res_need > 0 {
-        true => div_ceil(res_need, build.robots.clay_robots)?,
-        false => 1
-    };
+    // Advance the output state by the required amount of time, acquiring resources
     advance(&mut build, time);
-    build.ore -= scenario.clay_robot_ore;
-    build.robots.clay_robots += 1;
-    Some(build)
-}
-
-fn build_obsidian(from: &State, scenario: &Scenario) -> Option<State> {
-    let mut build = from.clone();
-    let res_need = scenario.obsidian_robot_ore.saturating_sub(build.ore);
-    let time = match res_need > 0 {
-        true => div_ceil(res_need, build.robots.ore_robots)?,
-        false => 1
-    };
-    let res_need = scenario.obsidian_robot_clay.saturating_sub(build.clay);
-    let time = max(time, match res_need > 0 {
-        true => div_ceil(res_need, build.robots.clay_robots)?,
-        false => 1
-    });
-    advance(&mut build, time);
-    build.ore -= scenario.obsidian_robot_ore;
-    build.clay -= scenario.obsidian_robot_clay;
-    build.robots.obsidian_robots += 1;
+    // Subtract required resources from accumulated
+    build
+        .resources
+        .iter_mut()
+        .zip(scenario.costs[robot_kind])
+        .for_each(|(res, robots)| *res -= robots);
+    // Build robot
+    build.robots[robot_kind] += 1;
     Some(build)
 }
 
-fn build_geode(from: &State, scenario: &Scenario) -> Option<State> {
-    let mut build = from.clone();
-    let res_need = scenario.geode_robot_ore.saturating_sub(build.ore);
-    let time = match res_need > 0 {
-        true => div_ceil(res_need, build.robots.ore_robots)?,
-        false => 1
-    };
-    let res_need = scenario.geode_robot_obsidian.saturating_sub(build.obsidian);
-    let time = max(time, match res_need > 0 {
-        true => div_ceil(res_need, build.robots.obsidian_robots)?,
-        false => 1
-    });
-    advance(&mut build, time);
-    build.ore -= scenario.geode_robot_ore;
-    build.obsidian -= scenario.geode_robot_obsidian;
-    build.robots.geode_robots += 1;
-    Some(build)
-}
-
-fn bfs_from(
-    max_time: usize,
-    start_state: State,
-    scenario: &Scenario,
-) -> usize {
-    let mut queue:VecDeque<_> = VecDeque::<State>::new();
-    let mut seen = HashMap::<RobotState, State>::new();
+fn bfs_from(max_time: usize, start_state: State, scenario: &Scenario) -> usize {
+    let mut queue: VecDeque<_> = VecDeque::<State>::new();
+    let mut seen = HashMap::<Res, State>::new();
     let mut best = 0_usize;
-    let mut count = 0_usize;
 
     queue.push_front(start_state);
 
     while queue.len() > 0 {
         let cur = queue.pop_front().unwrap();
-        count += 1;
         if cur.time >= max_time {
             continue;
         }
-        best = max(best, cur.geodes + (max_time - cur.time) * cur.robots.geode_robots);
-        // let (th, hh) = heuristic(&cur, max_time);
-        // match seen.get(&cur.robots) {
-        //     Some((t, h)) => if *t >= th && *h >= hh {continue;}
-        //     None => {}
-        // }
-        // seen.insert(cur.robots.clone(),heuristic(&cur, max_time));
+        best = max(
+            best,
+            cur.resources[Geode as usize] + (max_time - cur.time) * cur.robots[Geode as usize],
+        );
+
         match seen.get(&cur.robots) {
-            Some(t) => if better(t, &cur) {continue;}
+            Some(t) => {
+                if *t <= cur {
+                    continue;
+                }
+            }
             None => {}
         }
-        seen.insert(cur.robots.clone(),cur.clone());
+        seen.insert(cur.robots.clone(), cur.clone());
 
-        // if cur in seen continue
-        // insert cur nto seen
-
-        let next = State{
-            time: cur.time + 1,
-            robots: cur.robots.clone(),
-            ore: cur.ore + cur.robots.ore_robots,
-            clay: cur.clay + cur.robots.clay_robots,
-            obsidian: cur.obsidian + cur.robots.obsidian_robots,
-            geodes: cur.geodes + cur.robots.geode_robots,
-        };
-
-        // For each type of robot, see if the *current* stock is enough to build it.
-        // If so, clone the *next* state and subtract the stock, and add this to the queue.
-        // This is because paying happens at the start of the phase, but you get the robot at the end.
-        [build_ore, build_clay, build_obsidian, build_geode]
-            .iter()
-            .for_each(|f|
-                if let Some(state) = f(&cur, scenario) {
-                    queue.push_back(state)
-                }
-            );
-        // queue.push_back(build_ore(&cur, scenario));
-        // queue.push_back(build_clay(&cur, scenario));
-        // queue.push_back(build_obsidian(&cur, scenario));
-        // queue.push_back(build_geode(&cur, scenario));
-        // if cur.ore >= scenario.ore_robot_ore {
-        //     let mut build = next.clone();
-        //     build.ore -= scenario.ore_robot_ore;
-        //     build.robots.ore_robots += 1;
-        //     queue.push_back(build);
-        // }
-        // if cur.ore >= scenario.clay_robot_ore {
-        //     let mut build = next.clone();
-        //     build.ore -= scenario.clay_robot_ore;
-        //     build.robots.clay_robots += 1;
-        //     queue.push_back(build);
-        // }
-        // if cur.ore >= scenario.obsidian_robot_ore && cur.clay >= scenario.obsidian_robot_clay {
-        //     let mut build = next.clone();
-        //     build.ore -= scenario.obsidian_robot_ore;
-        //     build.clay -= scenario.obsidian_robot_clay;
-        //     build.robots.obsidian_robots += 1;
-        //     queue.push_back(build);
-        // }
-        // if cur.ore >= scenario.geode_robot_ore && cur.obsidian >= scenario.geode_robot_obsidian {
-        //     let mut build = next.clone();
-        //     build.ore -= scenario.geode_robot_ore;
-        //     build.obsidian -= scenario.geode_robot_obsidian;
-        //     build.robots.geode_robots += 1;
-        //     queue.push_back(build);
-        // }
-        // queue.push_back(next);
+        // try to build each kind of robot; if it works put it on the queue
+        Resource::iterator().copied().for_each(|f| {
+            if let Some(state) = build_robot(&cur, scenario, f) {
+                queue.push_back(state)
+            }
+        });
     }
-    println!("explored {} score {}", count, best);
     best
 }
 
 fn part1(scenarios: &Vec<Scenario>) {
     let score = scenarios
         .iter()
-        .map(|scenario|
-            bfs_from(
-                24,
-                State::new(1),
-                scenario
-            ) * scenario.id
-        )
-        .collect::<Vec<_>>();
-    // .sum();
+        .map(|scenario| bfs_from(24, State::new(1), scenario) * scenario.id)
+        .sum::<usize>();
     println!("{:?}", score);
 }
 
 fn part2(scenarios: &Vec<Scenario>) {
-    let score: usize = scenarios[0..max(2, scenarios.len() - 1)]
+    let score: usize = scenarios[0..min(3, scenarios.len() - 1)]
         .iter()
-        .map(|scenario|
-                 bfs_from(
-                     32,
-                     State::new(1),
-                     scenario
-                 ) * scenario.id
-        )
-        // .collect::<Vec<_>>();
+        .map(|scenario| bfs_from(32, State::new(1), scenario))
         .product();
     println!("{:?}", score);
 }
@@ -310,7 +202,6 @@ fn part2(scenarios: &Vec<Scenario>) {
 fn main() {
     let input = read_input().expect("Could not read input");
     let scenarios = parser::scenarios(&input).expect("Could not parse input");
-    println!("{:?}", scenarios);
     part1(&scenarios);
-    // part2(&scenarios);
+    part2(&scenarios);
 }
